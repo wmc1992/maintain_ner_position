9932a4d83cc0fcc46733bc21790bbf2c7844326b
王明超 <50796589+wmc1992@users.noreply.github.com>
2022-02-08 10:57:11 +0800

    2022-02-08 10:57:11

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+install_requires = [
+]
+
+setuptools.setup(
+    name="maintain_ner_position",
+    version="0.0.1",
+    license="MIT",
+    description="本项目提供几个简单的对原始文本做增删改的函数，同时能够维护其实体列表中的各索引同步修改。",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wmc1992/maintain_ner_position",
+    author="wmc1992",
+    author_email="m18810541081@163.com",
+    keywords="NER 索引",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    python_requires=">=3.6",
+)
